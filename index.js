02b90c2d3b618b66fa507e2bbf6f6f4feddd1582
nake89 <nake89@gmail.com>
2022-08-24 08:44:21 +0300

    added commands

--- index.js
+++ index.js
@@ -8,14 +8,39 @@ document.getElementById('input').addEventListener('keyup', (e) => {
   console.log(terminalContent)
   updateTerminalInput(e.target.value)
   if (e.key === 'Enter' || e.keyCode === 13) {
-    terminalContent.push(e.target.value)
-    let terminalContentHTML = terminalContent.map((x) => '$ ' + x).join('<br>')
+    terminalContent.push('$ ' + e.target.value)
+    command(e.target.value)
+    // let terminalContentHTML = terminalContent.map((x) => '$ ' + x).join('<br>')
+    let terminalContentHTML = terminalContent.join('<br>')
     document.getElementById('terminalContent').innerHTML = terminalContentHTML
     e.target.value = ''
     updateTerminalInput('')
     debug(e)
   }
 })
+
+function command(cmd) {
+  if (cmd === 'whoami') {
+    terminalContent.push('nake89')
+  } else if (cmd === 'clear') {
+    terminalContent = []
+  } else if (cmd === 'ls') {
+    terminalContent.push('about.txt intro.txt posts/ ')
+  } else if (cmd === 'pwd') {
+    terminalContent.push('/home/nake89')
+  } else if (cmd === 'ls -la') {
+    terminalContent.push('total 124')
+    terminalContent.push('drwxr-xr-x 31 nake89 nake89 4096 23. 8. 21:56 .')
+    terminalContent.push('drwx------ 38 nake89 nake89 4096 23. 8. 22:17 ..')
+    terminalContent.push(
+      '-rw-r--r--  1 nake89 nake89       431 11. 8. 11:44  about.txt'
+    )
+    terminalContent.push(
+      '-rw-r--r--  1 nake89 nake89       763 14. 8. 13:37  intro.txt'
+    )
+    terminalContent.push('drwxr-xr-x 10 nake89 nake89 4096  23. 8. 15:10 posts')
+  }
+}
 
 function updateTerminalInput(msg) {
   document.getElementById('terminal').innerText = '$ ' + msg
